d3f96955c364362de91c111d6d07cc963ba6c351
Coder7Raj <alwaysraj733@gmail.com>
2024-12-27 22:22:00 +0600

    car booking functionality successfully done

--- src/pages/CarDetails.jsx
+++ src/pages/CarDetails.jsx
@@ -3,6 +3,7 @@ import { Link, useLoaderData } from "react-router-dom";
 import { AuthContext } from "../auth/AuthProvider/AuthProvider";
 import { div } from "framer-motion/client";
 import { FaArrowLeft } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const CarDetails = () => {
   const carDetails = useLoaderData();
@@ -17,6 +18,34 @@ const CarDetails = () => {
     _id,
   } = carDetails;
   const { user } = useContext(AuthContext);
+
+  const carBookList = {
+    image,
+    name,
+    id: _id,
+    userEmail: user?.email,
+  };
+
+  const handleCarBooking = () => {
+    fetch(`http://localhost:5000/bookList/${_id}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(carBookList),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message) {
+          Swal.fire("This review already in watchlist");
+        }
+        if (data.insertedId) {
+          Swal.fire("Successfully added to GameWatchlist");
+        }
+      })
+      .catch((error) => console.error("Error adding to watchlist:", error));
+    //
+  };
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-gray-50 py-10 px-4">
       <div className="text-center mb-6">
@@ -65,7 +94,10 @@ const CarDetails = () => {
         </div>
         <div className="w-full p-4 bg-gray-100 border-t">
           <Link>
-            <button className="w-full btn btn-outline btn-primary">
+            <button
+              onClick={handleCarBooking}
+              className="w-full btn btn-outline btn-primary"
+            >
               Book Now
             </button>
           </Link>
